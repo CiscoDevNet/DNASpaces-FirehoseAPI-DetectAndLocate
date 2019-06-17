76f321753702868963aa55656a34c86e1216ed9f
thangs <thangs@cisco.com>
2019-06-17 17:44:49 +0530

    all configuration files removed

--- server/src/main/java/com/cisco/dnaspaces/utils/ConfigUtil.java
+++ server/src/main/java/com/cisco/dnaspaces/utils/ConfigUtil.java
@@ -1,10 +1,7 @@
 package com.cisco.dnaspaces.utils;
 
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Map;
 import java.util.Properties;
 
 /**
